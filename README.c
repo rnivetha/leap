66c8213b942b58be0df75ff2695c5a20b165b13d
rnivetha <rnivetha1995@gmail.com>
2016-07-08 23:34:32 -0700

    finish

---
+++ README.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int year;
+printf("Enter a year");
+scanf("%d",&year);
+if(year%4==0)
+printf("Leap year");
+else
+printf("Not a leap year");
+
+}
